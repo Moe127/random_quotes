c017281e2d99e6a9873e67543f3f2db9de2caa69
mohamed saada <mohamed.saada127@gmail.com>
2023-02-07 00:14:28 +0200

    added more quotes

--- main.js
+++ main.js
@@ -27,6 +27,32 @@ var quotes = [
       "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”",
     owner: "-- Albert Einstein",
   },
+  {
+    phrase:
+      "“ The future belongs to those who believe in the beauty of their dreams.”",
+    owner: "-- Hinata Shoyo",
+  },
+  {
+    phrase: " “I hate confidence that has no basis.”",
+    owner: "-- Ushijima Wakatoshi",
+  },
+  {
+    phrase:
+      "“Repetition, being methodical, and being through just feels nice.”",
+    owner: "-- Kita Shinsuke",
+  },
+  {
+    phrase: "“Being strong is being free.”",
+    owner: "-- Yamaguchi Tadashi",
+  },
+  {
+    phrase: "“Humans have no wings, so they search for a way to fly.”",
+    owner: "--  Ukai Keishin",
+  },
+  {
+    phrase: "“If you think that it will never come, it probably never will.”",
+    owner: "--  Oikawa Tooru",
+  },
 ];
 
 function getNewQuote() {
